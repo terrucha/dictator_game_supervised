1c5cb2c76613afa0f74149544eae82c0fad61cf9
DataTeller <151622487+morbifakhar69@users.noreply.github.com>
2025-07-28 23:53:22 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 export DATABASE_URL=${POSTGRESQL_ADDON_URI}
+echo "[INFO] Dropping and recreating public schema…"
+psql "$DATABASE_URL" -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO PUBLIC;"
+
 otree prodserver 9000
